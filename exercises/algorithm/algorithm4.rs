8d21934f539a7fc149f42d6a4d50661d6cc5c5a0
2563637107 <73688211+2563637107@users.noreply.github.com>
2024-04-28 15:00:55 +0000

    update

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,7 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
+
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -50,13 +50,16 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+       match self.root
+       {
+            Some(ref mut node) => node.insert(value),
+            None => self.root = Some(Box::new(TreeNode::<T>::new(value))),
+       }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        self.root.as_ref().map_or(false, |left| left.search(value))
     }
 }
 
@@ -66,7 +69,30 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+       {
+            match value.cmp(&self.value)
+            {
+                Ordering::Less => match self.left
+                {
+                    None => self.left =Some(Box::new(TreeNode::<T>::new(value))),
+                    Some(ref mut node) => node.insert(value)
+                },
+                Ordering::Equal =>(),
+                Ordering::Greater => match self.right
+                {
+                    None => self.right =Some(Box::new(TreeNode::<T>::new(value))),
+                    Some(ref mut node) => node.insert(value)
+                },
+            }
+       }
+    }
+    fn search(&self, value: T) -> bool {
+        match value.cmp(&self.value)
+        {
+            Ordering::Equal =>true,
+            Ordering::Greater =>self.right.as_ref().map_or(false, |right| right.search(value)),
+            Ordering::Less =>self.left.as_ref().map_or(false, |right| right.search(value)),
+        }
     }
 }
 
